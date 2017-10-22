c5ae9be24adac5e65a423e955f339754f61111a3
vgonisanz <vgonisanz@gmail.com>
2017-10-22 15:55:56 +0200

    server: First version with a server that accept http request to help path

--- README.md
+++ README.md
@@ -12,6 +12,92 @@ This bot use a submodule with [motion repository]()
 * **core**: Client api of linkero to make calls to running server and logic to manage bot behavior.
 * **server**: Api server of linkero to manage motion server.
 
+# Design v1
+
+All services can be requested using ```/api/v1```.
+* info: GET request to get info from the system.
+  * time: Get the current time.
+  * hw: Get hardware info.
+
+# Bot and client
+
+This bot use a token to be used by telegram api. Make http requests to server if is running.
+
+# Server
+
+Server is an instance running in host computer to manage motion library and usint its webcam as defined into configuration file.
+
+Launch server with command:
+```
+python server.py
+```
+
+Output logs from server example:
+```
+INFO:werkzeug: * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
+INFO:werkzeug:127.0.0.1 - - [01/Oct/2017 13:38:42] "GET /cmds HTTP/1.1" 200 -
+```
+
+* First line: Where is the server running
+* Second line: A GET HTTP request was caught to cmds URL.
+
+# How make client requests
+
+Exist several ways to use the server.
+
+* Curl: This executable (it can be used as library too) have a lot of options to make HTTP requests.
+* Python client script: Running client.py sample script, parse args to make requests.
+
+## Curl
+
+### Get request with JSON
+
+* Just running:
+```
+curl -i -X GET http://localhost:5000/v1/help
+```
+* More info:
+```
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/todos/todo1
+```
+
+### Port request
+
+* Just running:
+```
+curl -i -X POST http://localhost:5000/todos/todo1
+```
+
+### put
+
+Put is used to:
+```
+curl -X PUT -d arg=val -d arg2=val2 localhost:8080
+```
+
+### delete
+
+Delete is used to:
+```
+curl -i -X DELETE http://localhost:3001/api/v1/projects/559a328d8e67197a1c00d6dd
+```
+
+#### Using parameters
+
+curl --data "param1=value1&param2=value2" http://hostname/resource
+
+#### File upload
+
+curl --form "fileupload=@filename.txt" http://hostname/resource
+
+# Extra Data
+
+## Curl
+
+* -X: Specifies a custom request method to use when communicating with the HTTP server. Valid: GET, POST, PUT, DELETE
+* -H: Extra header to include in the request when sending HTTP to a server.
+* -i: Include the HTTP-header in the output.
+
 # Bibliography
 
 * [Python telegram bot wiki](https://github.com/python-telegram-bot/python-telegram-bot/wiki)
---
+++ server.py
@@ -0,0 +1,103 @@
+import linkero.core.linkero as linkero
+import linkero.core.gateway.gevent_service as gevent
+import linkero.core.gateway.waitress_service as waitress
+
+class CmdList(linkero.Resource):
+    """
+    CmdList linkero resource:
+        This class return a list with all commands defined for the API.
+        - Type request: GET, return complete JSON object.
+    """
+    versions_list = {
+        'v1': {'info': 'Initial version'},
+    }
+
+    def get(self):
+        return self.versions_list
+
+class CmdV1(linkero.Resource):
+    """
+    V1 class with
+    """
+
+    """ ********************************************************************** """
+    """ ******                   HTTP    functions                 *********** """
+    """ ********************************************************************** """
+
+    def get(self, cmd):
+        value = ''
+        if is_cmd_in_list(self.v1_cmd_list, cmd):
+            value = self.v1_cmd_list[cmd]()         # Invoke callback for each command
+        return value, 201
+
+    def delete(self, cmd):
+        #is_cmd_in_list(v1_cmd_list, cmd)
+        #del versions_list[cmd]
+        return '', 204
+
+    def put(self, cmd):
+        #args = parser.parse_args()
+        #task = {'task': args['task']}
+        #versions_list[cmd] = task
+        return task, 201
+
+    """ ********************************************************************** """
+    """ ******                  Internal functions                 *********** """
+    """ ********************************************************************** """
+    def f_help(self):
+        print("f_help")
+        return
+
+    def f_info(self):
+        print("f_info")
+        return
+
+    v1_cmd_list = {
+        'help': f_help,
+        'info': f_info
+    }
+
+
+class Server(object):
+    """
+    This python 3 class manage server with user's motion execution.
+
+    To generate HTML documentation for this module issue the command: pydoc -w Core
+    """
+
+    """ ********************************************************************** """
+    """ ******                   Internal functions                *********** """
+    """ ********************************************************************** """
+
+    """
+    Initialize class: Initialize CursesManager
+    """
+    def __init__(self):
+        """
+        load: Api load add all server resources to get them ready to be used with HTTP requests.
+                - help: This resource return all versions valid.
+        """
+        linkero.api.add_resource(CmdList, '/help')  # Call CmdList resource if HTTP request to help
+        #linkero.api.add_resource(CmdV1, '/v1/<cmd>')
+        return
+
+    def __enter__(self):
+        return
+
+    def __del__(self):
+        # Catch any weird termination situations
+        return
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return
+
+    def run(self):
+        linkero.run()  # Run with Werkzeug (not recommended for production environments)
+        # gevent.run(linkero.app)    # Run with Gevent
+        # waitress.run(linkero.app)   # Run with Waitress
+        return
+
+if __name__ == '__main__':
+    # Entry point
+    server = Server()
+    server.run()
