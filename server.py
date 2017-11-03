deb890e7a556a23c598da778d8557f87b2ee77a2
vgonisanz <vgonisanz@gmail.com>
2017-11-03 17:02:04 +0100

    server: Update info and help methods

--- server.py
+++ server.py
@@ -25,6 +25,9 @@ class CmdV1(linkero.Resource):
     of this class.
     """
     version = 'Server API V1.0'
+    info = """This server run an instance to manage local motion software. 
+            in order to use, send a request to help to get a list of commands. 
+            This software has been develop by Victor Goni Sanz. """
 
     """ ********************************************************************** """
     """ ******                   HTTP    functions                 *********** """
@@ -33,9 +36,9 @@ class CmdV1(linkero.Resource):
     def get(self, cmd):
         value = ''
         if self.is_cmd_in_list(self.v1_cmd_list, cmd):
-            value = self.v1_cmd_list[cmd]()         # Invoke callback for each command
+            value = list(self.v1_cmd_list[cmd].keys())[0](self)         # Invoke callback for each command
         else:
-            value = 'No valid command for v1'
+            value = 'No valid command for v1. Use help command to check valid cmds'
         return value, 201
 
     def delete(self, cmd):
@@ -66,31 +69,28 @@ class CmdV1(linkero.Resource):
     ###############################################################
     # Internal <function_call> from v1_cmd_list, modify to add behavior.
     ###############################################################
-    def f_help():
-        print("f_help")
-        return 'f_help'
+    def f_help(self):
+        message = ''
+        for value in self.v1_cmd_list:
+            message = message + 'Command: <' + str(value) + '>: ' + self.v1_cmd_list[value].values()[0] + '. \n'
+        return message
 
-    def f_info():
-        print("f_info")
-        return 'f_info'
+    def f_info(self):
+        return self.info
 
-    def f_start():
+    def f_start(self):
         print("f_start")
         return 'f_start'
 
-    def f_stop():
+    def f_stop(self):
         print("f_stop")
         return 'f_stop'
 
-    def f_version():
+    def f_version(self):
         print("f_version")
-        return 'f_f_versionf_infoinfo'
+        return self.version
 
-    def f_info():
-        print("f_info")
-        return 'f_info'
-
-    def f_test():
+    def f_test(self):
         print("f_test")
         return 'f_test'
 
@@ -98,13 +98,12 @@ class CmdV1(linkero.Resource):
     # Array with <cmd>:<function_call> modify to add cmds.
     ###############################################################
     v1_cmd_list = {
-        'help': f_help,
-        'info': f_info,
-        'start': f_start,
-        'stop': f_stop,
-        'info': f_info,
-        'version': f_version,
-        'test': f_test
+        'help': {f_help: 'Show all commands available'},
+        'start': {f_start: 'Start motion binary to detect events. If is already active do nothing.'},
+        'stop': {f_stop: 'Stop motion binary detecting events. If no active do nothing.'},
+        'info': {f_info: 'Print info about author and implementation'},
+        'version': {f_version: 'Print version info'},
+        'test': {f_test: 'Testing tochas stuffs'},
     }
 
 class Server(object):
